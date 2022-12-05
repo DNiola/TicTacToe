03087117c09af088ce4a06c420c850d64172960e
Davide Niola <davide.n2@gmx.de>
2022-12-05 23:08:46 +0100

    Update Style. Beta 0.3.0

--- script.js
+++ script.js
@@ -50,44 +50,47 @@ function checkForWin() {
   let winner;
   if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
     winner = fields[0];
-    document.getElementById("winnerLine1").style.transform = "scaleX(1)";
+    document.getElementById("winnerLine1").style.transform = "scaleX(1.2)";
   }
 
   if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
     winner = fields[3];
-    document.getElementById("winnerLine2").style.transform = "scaleX(1)";
+    document.getElementById("winnerLine2").style.transform = "scaleX(1.2)";
   }
   if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
     winner = fields[6];
-    document.getElementById("winnerLine3").style.transform = "scaleX(1)";
+    document.getElementById("winnerLine3").style.transform = "scaleX(1.2)";
   }
 
   if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
     winner = fields[0];
     document.getElementById("winnerLine4").style.transform =
-      "rotate(90deg) scaleX(1)";
+      "rotate(90deg) scaleX(1.2)";
   }
   if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
     winner = fields[1];
     document.getElementById("winnerLine5").style.transform =
-      "rotate(90deg) scaleX(1)";
+      "rotate(90deg) scaleX(1.2)";
   }
 
   if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
     winner = fields[2];
     document.getElementById("winnerLine6").style.transform =
-      "rotate(90deg) scaleX(1)";
+      "rotate(90deg) scaleX(1.3)";
   }
   if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
     winner = fields[0];
     document.getElementById("winnerLine7").style.transform =
-      "rotate(45deg) scaleX(1.2)";
+      "rotate(45deg) scaleX(1.4)";
   }
 
   if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
     winner = fields[2];
     document.getElementById("winnerLine8").style.transform =
-      "rotate(-45deg) scaleX(1.2)";
+      "rotate(-45deg) scaleX(1.4)";
+  }
+  if (fields.length === 9 ){
+    console.log("Kein GEWINNER:");
   }
   if (winner) {
     console.log("GEWINNER:", winner);
