f0c2ff26ca25dfb6e29100bd3b80c60c9744b9da
Davide Niola <davide.n2@gmx.de>
2022-12-05 23:59:07 +0100

    Update No winner action Beta 0.3.1

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let fields = [];
 let gameOver = false;
 let currentShape = "cross";
+let rounds =[];
 
 function fillShape(id) {
   if (!fields[id] && !gameOver) {
@@ -23,10 +24,12 @@ function fillShape(id) {
 function restart() {
   gameOver = false;
   fields = [];
+  rounds = [];
+  document.getElementById("noWinner").classList.add("d-none");
   document.getElementById("gameOver").classList.add("d-none");
   document.getElementById("restartBtn").classList.add("d-none");
 
-  for (let i = 1; i < 8; i++) {
+  for (let i = 1; i < 9; i++) {
     document.getElementById("winnerLine" + i).classList.add("d-none");
   }
   for (let i = 0; i < 9; i++) {
@@ -47,57 +50,71 @@ function draw() {
 }
 
 function checkForWin() {
+  rounds++
   let winner;
   if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
     winner = fields[0];
     document.getElementById("winnerLine1").style.transform = "scaleX(1.2)";
+      document.getElementById("winnerLine1").classList.remove('d-none') ;
   }
 
   if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
     winner = fields[3];
     document.getElementById("winnerLine2").style.transform = "scaleX(1.2)";
+      document.getElementById("winnerLine2").classList.remove('d-none') ;
   }
   if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
     winner = fields[6];
     document.getElementById("winnerLine3").style.transform = "scaleX(1.2)";
+      document.getElementById("winnerLine3").classList.remove('d-none') ;
   }
 
   if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
     winner = fields[0];
-    document.getElementById("winnerLine4").style.transform =
-      "rotate(90deg) scaleX(1.2)";
+    document.getElementById("winnerLine4").style.transform = "rotate(90deg) scaleX(1.2)";
+      document.getElementById("winnerLine4").classList.remove('d-none') ;
   }
   if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
     winner = fields[1];
-    document.getElementById("winnerLine5").style.transform =
-      "rotate(90deg) scaleX(1.2)";
+    document.getElementById("winnerLine5").style.transform = "rotate(90deg) scaleX(1.2)";
+      document.getElementById("winnerLine5").classList.remove('d-none') ;
   }
 
   if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
     winner = fields[2];
-    document.getElementById("winnerLine6").style.transform =
-      "rotate(90deg) scaleX(1.3)";
+    document.getElementById("winnerLine6").style.transform = "rotate(90deg) scaleX(1.3)";
+      document.getElementById("winnerLine6").classList.remove('d-none') ;
   }
   if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
     winner = fields[0];
-    document.getElementById("winnerLine7").style.transform =
-      "rotate(45deg) scaleX(1.4)";
+    document.getElementById("winnerLine7").style.transform = "rotate(45deg) scaleX(1.6)";
+      document.getElementById("winnerLine7").classList.remove('d-none') ;
   }
 
   if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
     winner = fields[2];
-    document.getElementById("winnerLine8").style.transform =
-      "rotate(-45deg) scaleX(1.4)";
+    document.getElementById("winnerLine8").style.transform = "rotate(-45deg) scaleX(1.6)";
+      document.getElementById("winnerLine8").classList.remove('d-none') ;
   }
-  if (fields.length === 9 ){
+  if (rounds > 8 && winner === undefined){
     console.log("Kein GEWINNER:");
+    gameOver = true;
+    setTimeout(function () {
+      document.getElementById("noWinner").classList.remove("d-none");
+    }, 1000);
+    setTimeout(function () {
+      document.getElementById("gameOver").classList.remove("d-none");
+      document.getElementById("restartBtn").classList.remove("d-none");
+    }, 1075);
   }
   if (winner) {
     console.log("GEWINNER:", winner);
     gameOver = true;
     setTimeout(function () {
       document.getElementById("gameOver").classList.remove("d-none");
-      document.getElementById("restartBtn").classList.remove("d-none");
     }, 1000);
+    setTimeout(function () {
+      document.getElementById("restartBtn").classList.remove("d-none");
+    }, 1075);
   }
 }
